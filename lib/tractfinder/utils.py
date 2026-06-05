3ba9f853a493617e90dc1fa0aa9bae0765055d08
Jon Clayden <code@clayden.org>
2026-06-05 15:14:30 +0100

    Add Tractfinder-specific copyright statement; update references

--- lib/tractfinder/utils.py
+++ lib/tractfinder/utils.py
@@ -1,3 +1,14 @@
+# Tractfinder is Copyright (c) 2023-2026 Fiona Young, and (c) 2026 Tractfinder
+# contributors. The software is distributed "as is", without any warranty, for
+# non-commercial use only. Any commercial use, including incorporation into a
+# product for sale, or use of the software to provide paid services, is not
+# permitted without a commercial license. For licensing enquiries please
+# contact UCL Business Ltd (https://www.uclb.com).
+# 
+# Tractfinder may include redistributed source files from MRtrix3
+# (https://www.mrtrix.org/), which are subject to separate licensing terms.
+# Details can be found in the relevant files.
+
 import sys
 import numpy as np
 from mrtrix3 import app, ANSI
